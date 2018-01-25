269826c6d0d1d8d6f7c2938eeaebb8bf7b6b5c19
colcampbe3 <cccampbell@oakland.edu>
2018-01-25 12:42:01 -0500

    Add BeerSong class

--- BeerSong.java
+++ BeerSong.java
@@ -1,3 +1,5 @@
-public static void main(String[] args){}
+public class BeerSong{
+    public static void main(String[] args){}
 
-public static void Ninety_Nine_Bottles_of_Beer(){}
+    public static void Ninety_Nine_Bottles_of_Beer(){}
+}
