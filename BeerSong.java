4c174c6585365419d5ca008d4bd87e7bcb55ec32
colcampbe3 <cccampbell@oakland.edu>
2018-01-25 12:48:32 -0500

    Implement tested song method

--- BeerSong.java
+++ BeerSong.java
@@ -3,5 +3,15 @@ public class BeerSong{
         Ninety_Nine_Bottles_of_Beer();
     }
 
-    public static void Ninety_Nine_Bottles_of_Beer(){}
+    public static void Ninety_Nine_Bottles_of_Beer(){
+        int i = 99;
+        while(i > 2){
+            System.out.println(i + " bottles of beer on the wall, " + i-- + " bottles of beer");
+            System.out.println("Take one down, pass it around, " + i + " bottles of beer on the wall.");        
+        }
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer");
+        System.out.println("Take one down, pass it around, 1 bottle of beer on the wall.");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer");
+        System.out.println("Take one down, pass it around, no more bottles of beer on the wall.");
+    }
 }
