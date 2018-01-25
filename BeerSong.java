a3e0d2b09342bccab6561e757ecb5c95c60af0b2
colcampbe3 <cccampbell@oakland.edu>
2018-01-25 12:44:49 -0500

    Add implementation to main method

--- BeerSong.java
+++ BeerSong.java
@@ -1,5 +1,7 @@
 public class BeerSong{
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Ninety_Nine_Bottles_of_Beer();
+    }
 
     public static void Ninety_Nine_Bottles_of_Beer(){}
 }
